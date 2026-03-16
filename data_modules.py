aeb7978ca3288ac6f34a8c250ddd6a90f4db9215
shufanshahi <shufanshahi@gmail.com>
2026-03-16 13:34:44 +0600

    data and helper modules written

---
+++ data_modules.py
@@ -0,0 +1,239 @@
+import torch
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+import pickle, pandas as pd
+import numpy as np
+
+class IEMOCAP4(Dataset):
+
+    def __init__(self, path, train= True):
+        with open(path, 'rb') as f:
+            data = pickle.load(f, encoding='latin1')
+        (
+            self.videoIDs,
+            self.videoSpeakers,
+            self.videoLabels,
+            self.videoText,
+            self.videoAudio,
+            self.videoVisual,
+            self.videoSentence,
+            self.trainVid,
+            self.testVid,
+        ) = data
+
+        if(train):
+            self.currentVideos = self.trainVid
+        else:
+            self.currentVideos = self.testVid
+        
+        self.length = len(self.currentVideos)
+        self.emotions = self.videoLabels
+        
+        sentiments_dict = {}
+
+        for i in self.videoLabels:
+            dialogue_sentiments = []
+            for j in self.videoLabels[i]:
+                if j in [1, 3]:
+                    dialogue_sentiments.append(0)
+                elif j in [2]:
+                    dialogue_sentiments.append(1)
+                elif j in [0]:
+                    dialogue_sentiments.append(2)
+            sentiments_dict[i] = dialogue_sentiments
+        self.sentiments = sentiments_dict
+
+
+    def __getitem__(self, index):
+        
+        videoID_list = self.currentVideos[index]
+
+        return (
+            torch.FloatTensor(np.array(self.videoText[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText[videoID_list])),
+            torch.FloatTensor(np.array(self.videoVisual[videoID_list])),
+            torch.FloatTensor(np.array(self.videoAudio[videoID_list])),
+            torch.FloatTensor(
+                [
+                    [1, 0] if x == "M" else [0, 1]
+                    for x in np.array(self.videoSpeakers[videoID_list])
+                ]
+            ),
+            torch.FloatTensor([1] * len(np.array(self.emotions[videoID_list]))),
+            torch.LongTensor(np.array(self.emotions[videoID_list])),
+            torch.LongTensor(np.array(self.sentiments[videoID_list])),
+            videoID_list,
+        )
+
+    def __len__(self):
+        return self.length
+    
+    def padding_funtion(self, data):
+
+        unpadded_data = pd.DataFrame(data)
+        result = []
+
+        for i in unpadded_data:
+            if i < 10:
+                result.append(pad_sequence(unpadded_data[i]))
+            else:
+                result.append(unpadded_data[i].tolist())
+        
+        return result
+
+
+class IEMOCAP(Dataset):
+
+    def __init__(self, path, train= True):
+        with open(path, 'rb') as f:
+            data = pickle.load(f, encoding='latin1')
+        (
+            self.videoIDs,
+            self.videoSpeakers,
+            self.videoLabels,
+            self.videoText0,
+            self.videoText1,
+            self.videoText2,
+            self.videoText3,
+            self.videoAudio,
+            self.videoVisual,
+            self.videoSentence,
+            self.trainVid,
+            self.testVid,
+        ) = data
+
+        if(train):
+            self.currentVideos = self.trainVid
+        else:
+            self.currentVideos = self.testVid
+        
+        self.length = len(self.currentVideos)
+        self.emotions = self.videoLabels
+        sentiments_dict = {}
+
+        for i in self.videoLabels:
+            dialogue_sentiments = []
+            for j in self.videoLabels[i]:
+                if j in [1, 3, 5]:
+                    dialogue_sentiments.append(0)
+                elif j in [2]:
+                    dialogue_sentiments.append(1)
+                elif j in [0, 4]:
+                    dialogue_sentiments.append(2)
+            sentiments_dict[i] = dialogue_sentiments
+        self.sentiments = sentiments_dict
+
+    def __getitem__(self, index):
+        
+        videoID_list = self.currentVideos[index]
+
+        return (
+            torch.FloatTensor(np.array(self.videoText0[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText1[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText2[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText3[videoID_list])),
+            torch.FloatTensor(np.array(self.videoVisual[videoID_list])),
+            torch.FloatTensor(np.array(self.videoAudio[videoID_list])),
+            torch.FloatTensor(
+                [
+                    [1, 0] if x == "M" else [0, 1]
+                    for x in np.array(self.videoSpeakers[videoID_list])
+                ]
+            ),
+            torch.FloatTensor([1] * len(np.array(self.emotions[videoID_list]))),
+            torch.LongTensor(np.array(self.emotions[videoID_list])),
+            torch.LongTensor(np.array(self.sentiments[videoID_list])),
+            videoID_list,
+        )
+    
+    def __len__(self):
+        return self.length
+    
+    def padding_funtion(self, data):
+        
+        unpadded_data = pd.DataFrame(data)
+        result = []
+
+        for i in unpadded_data:
+            if i < 10:
+                result.append(pad_sequence(unpadded_data[i]))
+            else:
+                result.append(unpadded_data[i].tolist())
+        
+        return result
+    
+class MELD(Dataset):
+    
+    def __init__(self, path, train= True):
+        with open(path, 'rb') as f:
+            data = pickle.load(f, encoding='latin1')
+        (
+            self.videoIDs,
+            self.videoSpeakers,
+            self.videoLabels,
+            self.videoSentiments,
+            self.videoText0,
+            self.videoText1,
+            self.videoText2,
+            self.videoText3,
+            self.videoAudio,
+            self.videoVisual,
+            self.videoSentence,
+            self.trainVid,
+            self.testVid,
+            _,
+        ) = data
+
+        if(train):
+            self.currentVideos = list(self.trainVid)
+        else:
+            self.currentVideos = list(self.testVid)
+        
+        self.length = len(self.currentVideos)
+        self.emotions = self.videoLabels
+        self.sentiments = self.videoSentiments
+
+    def __getitem__(self, index):
+        
+        videoID_list = self.currentVideos[index]
+
+        return (
+            torch.FloatTensor(np.array(self.videoText0[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText1[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText2[videoID_list])),
+            torch.FloatTensor(np.array(self.videoText3[videoID_list])),
+            torch.FloatTensor(np.array(self.videoVisual[videoID_list])),
+            torch.FloatTensor(np.array(self.videoAudio[videoID_list])),
+            torch.FloatTensor(np.array(self.videoSpeakers[videoID_list])),
+            torch.FloatTensor([1] * len(np.array(self.emotions[videoID_list]))),
+            torch.LongTensor(np.array(self.emotions[videoID_list])),
+            torch.LongTensor(np.array(self.sentiments[videoID_list])),
+            videoID_list,
+        )
+    
+    def __len__(self):
+        return self.length
+    
+
+    def return_emotions(self):
+        return_emotions = []
+        for videoIds in self.currentVideos:
+            return_emotions += self.videoLabels[videoIds]
+        return return_emotions
+    
+    def padding_funtion(self, data):
+        
+        unpadded_data = pd.DataFrame(data)
+        result = []
+
+        for i in unpadded_data:
+            if i < 10:
+                result.append(pad_sequence(unpadded_data[i]))
+            else:
+                result.append(unpadded_data[i].tolist())
+        
+        return result
+
+    
---
+++ helpers.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+from itertools import permutations, product
+import math
+import copy
+from torch.nn import Parameter
+
+def build_tva_node_feature(text_features, video_features, audio_features, lengths):
+    text_nodes = []
+    audio_nodes = []
+    video_nodes = []
+
+    batch_size = text_features.size(1)
+
+    for idx in range(batch_size):
+        text_nodes.append(text_features[:lengths[idx], idx, :])
+        audio_nodes.append(audio_features[:lengths[idx], idx, :])
+        video_nodes.append(video_features[:lengths[idx], idx, :])
+
+    text_nodes = torch.cat(text_nodes, dim=0).cuda()
+    audio_nodes = torch.cat(audio_nodes, dim=0).cuda()
+    video_nodes = torch.cat(video_nodes, dim=0).cuda()
+
+    return text_nodes, audio_nodes, video_nodes
+
+
+class DynamicWeightedLoss(nn.Module):
+    def __init__(self, sigma_count):
+        super(DynamicWeightedLoss, self).__init__()
+        sigma = torch.ones(sigma_count, requires_grad=True)
+        self.sigma = torch.nn.Parameter(sigma)
+
+    def forward(self, *losses):
+        total_loss = 0
+        for i in range(len(losses)):
+            total_loss += (0.5 / (self.sigma[i] ** 2)) * losses[i] + torch.log(self.sigma[i] ** 2)
+        return total_loss
+    
+
+
+def _get_clones(module, layer_num):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(layer_num)])
+
+
+
+
+class Heterogeneous_Graph(torch.nn.Module):
+    def __init__(self, input_feature_size, dropout=0.3, no_cuda=False):
+        super(Heterogeneous_Graph, self).__init__()
+        self.no_cuda = no_cuda
+        self.heterogeneous_gcn = Simple_GCN(input_feature_size, input_feature_size)
+
+    def forward(self, feature, modality_number, adjencency_matrix):
+        if modality_number > 1:
+            heterogeneous_feature = self.heterogeneous_gcn(feature, adjencency_matrix)
+            return heterogeneous_feature
+        else:
+            print("No need for heterogeneous graph when there is only one modality")
+            return feature      
+
+class Simple_GCN(torch.nn.Module):
+    def __init__(self, input_feature, output_feature, bias=True):
+        super(Simple_GCN, self).__init__()
+        self.input_feature = input_feature
+        self.output_feature = output_feature
+        self.weight = Parameter(torch.FloatTensor(input_feature, output_feature))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(output_feature))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input_feature, adj):
+        try:
+            input_feature = input_feature.float()
+        except:
+            pass
+        support = torch.mm(input_feature, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
